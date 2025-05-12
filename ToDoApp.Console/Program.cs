80a2801c8b8e22110dcc1fd322c873eb07739d17
Nirojan Arunaparan <nirojan.arun@gmail.com>
2025-05-12 11:10:00 -0400

    Add Dependency Injection

--- ToDoApp.Console/Program.cs
+++ ToDoApp.Console/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ToDoApp.Console1;
+
 Console.WriteLine("Hello, World!");
+
+//Create service collection for Dependency Injection
+var serviceCollection = new ServiceCollection();
+
+//Build a configuration
+IConfiguration configuration;
+configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .Build();
+
+//Add configuration to the service collection
+serviceCollection.AddSingleton<IConfiguration>(configuration);
+serviceCollection.AddSingleton<Test>();
+
+//Test the configuration
+var serviceProvider = serviceCollection.BuildServiceProvider();
+var testInstance = serviceProvider.GetService<Test>();
+testInstance.TestMethod();
---
+++ ToDoApp.Console/Test.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Console1
+{
+    internal class Test
+    {
+        private readonly IConfiguration _configuration;
+
+        public Test(IConfiguration congfiguration)
+        {
+            this._configuration = congfiguration;
+        }
+
+        public void TestMethod()
+        {
+            // Access the configuration value
+            var dataFromJsonFile = _configuration.GetSection("FileName").Value;
+            Console.WriteLine(dataFromJsonFile);
+        }
+    }
+}
