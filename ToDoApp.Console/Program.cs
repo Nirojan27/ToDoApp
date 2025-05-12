a9be3bae262914709f51a597e02bdb8e0954b8fb
Nirojan Arunaparan <nirojan.arun@gmail.com>
2025-05-12 16:31:39 -0400

    DI for IToDoRepository, IToDoService in console

--- ToDoApp.Console/Program.cs
+++ ToDoApp.Console/Program.cs
@@ -1,7 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ToDoApp.Application.Services;
 using ToDoApp.Console1;
+using ToDoApp.Domain.Interfaces;
+using ToDoApp.Domain.Repositories;
+using ToDoApp.Infrastructure.Repositories;
 
 Console.WriteLine("Hello, World!");
 
@@ -17,6 +21,8 @@ configuration = new ConfigurationBuilder()
 
 //Add configuration to the service collection
 serviceCollection.AddSingleton<IConfiguration>(configuration);
+serviceCollection.AddSingleton<IToDoRepository, ToDoRepository>();
+serviceCollection.AddSingleton<IToDoService, ToDoService>();
 serviceCollection.AddSingleton<Test>();
 
 //Test the configuration
