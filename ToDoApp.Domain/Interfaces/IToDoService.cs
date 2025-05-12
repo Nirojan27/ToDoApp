20901309e48c1e6de06f0241f285215b76891c6f
Nirojan Arunaparan <nirojan.arun@gmail.com>
2025-05-12 11:30:18 -0400

    Add ToDoItem class/Add IToDoService interface

---
+++ ToDoApp.Domain/Interfaces/IToDoService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Domain.Entities;
+
+namespace ToDoApp.Domain.Interfaces
+{
+    public interface IToDoService
+    {
+        void AddTask(string title, string? description, DateTime? dueDate);
+        void CompleteTask(int id);
+        void IncompleteTask(int id);
+        void UpdateTask(int id, string? title, string? description, DateTime? dueDate);
+        void DeleteTask(int id);
+        List<ToDoItem> GetTasks();
+        ToDoItem? GetTaskById(int id);
+    }
+}
